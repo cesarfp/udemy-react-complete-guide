c54e4a3a6eda2fb5470b522bc84f3ca335ab6488
César <cesar.franco@4thsource.com>
2019-03-05 09:46:28 -0600

    class 95

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,20 +1,27 @@
-import React, {Component} from 'react'
+import React, {PureComponent} from 'react'
 import Person from './Person/Person'
 
-class Persons extends Component {
+class Persons extends PureComponent {
 	// static getDerivedStateFromProps(props,state){
 	// 	console.log('[Persons.js] getDerivedStateFromProps');
 	// 	return state;
 	// }
 
-	shouldComponentUpdate(nextProps, nextState){
-		console.log('[Persons.js] shouldComponentUpdate');
-		if(nextProps.persons !== this.props.persons){
-			return true;	
-		} 
-		else
-			return false;
-	}
+	// shouldComponentUpdate(nextProps, nextState){
+	// 	console.log('[Persons.js] shouldComponentUpdate');
+	// 	if( nextProps.persons !== this.props.persons || 
+	// 		nextProps.changed !== this.props.changed ||
+	// 		nextProps.clicked !== this.props.clicked ) 
+	// 	{
+	// 		console.log('[Persons.js] shouldComponentUpdate true');
+	// 		return true;
+	// 	}
+
+	// 	else{
+	// 		console.log('[Persons.js] shouldComponentUpdate false');
+	// 		return false;
+	// 	}
+	// }
 
 	getSnapshotBeforeUpdate(prevProps, prevState){
 		console.log('[Persons.js] getSnapshotBeforeUpdate')
