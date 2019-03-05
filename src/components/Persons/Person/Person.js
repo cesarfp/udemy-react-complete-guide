961e2f4c50d42f40e6ece35464dbbf1931f48012
César <cesar.franco@4thsource.com>
2019-03-05 12:52:11 -0600

    class 97

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,21 +1,36 @@
 import React, {Component} from 'react';
-
-import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary'
+// import classes from './Person.css';
 
 class Person extends Component {
     render( ){
         console.log('[Person.js] rendering...');
-        return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
+        return(
+            <Aux>
+                <p onClick={this.props.click}>
+                    I'm a {this.props.name} and I'm {this.props.age} years old
+                </p>
                 <p>{this.props.children}</p>
-                <input 
+                <input
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name}
                 />
-            </div>
-        )
+            </Aux>
+        );
+            
+        
+        // return (
+        //     <div className={classes.Person}>
+        //         <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old</p>
+        //         <p>{this.props.children}</p>
+        //         <input 
+        //             type="text" 
+        //             onChange={this.props.changed} 
+        //             value={this.props.name}
+        //         />
+        //     </div>
+        // )
     }
 }
 
