56540aa679c1f00d98e0f109b24d4dac7363b732
César <cesar.franco@4thsource.com>
2019-03-05 15:59:13 -0600

    class 101

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit'
-import WithClass from '../hoc/WithClass'
+import withClass from '../hoc/WithClass'
+import Aux from '../hoc/Auxiliary'
 class App extends Component {
 	
 	constructor(props){
@@ -91,7 +92,7 @@ class App extends Component {
 
 		return (
 			
-			<WithClass classes={classes.App}>
+			<Aux>
 				<button
 					onClick={()=>{this.setState({showCockpit:false})}}>
 					Remove Cockpit
@@ -102,10 +103,10 @@ class App extends Component {
 					personsLength={this.state.persons.length} 
 					clicked={this.togglePersonHandler}/> : null} 
 				{persons}
-			</WithClass>
+			</Aux>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 	}
 }
 
-export default App;
+export default withClass(App, classes.App);
--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
-const withClass = props => (
-	<div className={props.classes}>
-		{props.children}
-	</div>
-);
+const withClass = (WrappedComponent, className) => {
+	return props => (
+		<div className={className}>
+			<WrappedComponent/>
+		</div>
+	)
+}
+
+
+// const withClass = props => (
+// 	<div className={props.classes}>
+// 		{props.children}
+// 	</div>
+// );
 
 export default withClass;
